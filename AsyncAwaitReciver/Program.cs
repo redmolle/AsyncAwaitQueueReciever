8742ed79e18c1ca2fe03f8a263f7cf0422af32db
redmolle <redmolle@gmail.com>
2019-04-20 15:25:46 +0300

    Done Simple Async Reciever

--- AsyncAwaitReciver/Program.cs
+++ AsyncAwaitReciver/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Messaging;
 
 namespace AsyncAwaitReciver
 {
@@ -10,6 +8,39 @@ namespace AsyncAwaitReciver
     {
         static void Main(string[] args)
         {
+            Process();
+            while(true)
+            {
+                Print("Some working...");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        static async Task Process()
+        {
+            MessageQueue mq = new MessageQueue(@".\private$\MyPrivateQ");
+            mq.Formatter = new XmlMessageFormatter(
+                new String[] {
+                    "System.String",
+                    "System.Single"
+                });
+            try
+            {
+                Message msg = await Task.Factory.FromAsync<Message>(
+                  mq.BeginReceive(),
+                  mq.EndReceive);
+                Print(msg.Body.ToString());
+            }
+            catch
+            {
+                Print("Timeout!");
+            }
+            await Process();
+        }
+
+        static void Print(string msg)
+        {
+            Console.WriteLine(msg);
         }
     }
 }
